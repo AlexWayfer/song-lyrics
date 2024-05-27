bb9ca7c3b3b49b8718cace015cb6c5d467f0fc55
Alexander Popov <alex.wayfer@gmail.com>
2024-05-28 00:07:55 +0300

    Add moving by header for popup iframe

--- scripts/src/service-worker.js
+++ scripts/src/service-worker.js
@@ -18,10 +18,12 @@ chrome.action.onClicked.addListener(async tab => {
 			const
 				container = document.createElement('div'),
 				containerWidth = '500px',
+				containerHeader = document.createElement('h1'),
 				iframe = document.createElement('iframe')
 
 			container.style = `
 				display: flex;
+				flex-direction: column;
 				resize: both;
 				overflow: auto;
 				position: fixed;
@@ -29,18 +31,61 @@ chrome.action.onClicked.addListener(async tab => {
 				left: calc(100vw - ${containerWidth} - 50px);
 				width: ${containerWidth};
 				height: 600px;
-				border: 3px solid var(--border-color);
+				border-width: 3px;
+				border-style: solid;
+				border-color: transparent;
 				user-select: none;
 				z-index: 999999999;
 			`
 
 			container.id = 'song-lyrics-container'
 
+			containerHeader.innerText = 'Song Lyrics'
+			containerHeader.style.fontSize = '16px'
+			containerHeader.style.fontWeight = 'bold'
+			containerHeader.style.padding = '0.1em 0.4em 0.4em'
+			containerHeader.style.cursor = 'move'
+
+			container.draggingEvent = event => {
+				const
+					containerRect = container.getBoundingClientRect(),
+					newLeft = containerRect.left + event.clientX - container.lastPosition.left,
+					newTop = containerRect.top + event.clientY - container.lastPosition.top
+
+				container.style.left = `${newLeft}px`
+				container.style.top = `${newTop}px`
+
+				container.lastPosition = { left: event.clientX, top: event.clientY }
+			}
+
+			containerHeader.addEventListener('mousedown', event => {
+				container.lastPosition = { left: event.clientX, top: event.clientY }
+
+				containerHeader.addEventListener('mousemove', container.draggingEvent)
+			})
+
+			containerHeader.addEventListener('mouseup', _event => {
+				// console.debug('containerHeader mouseup')
+
+				container.lastPosition = null
+
+				container.removeEventListener('mousemove', container.draggingEvent)
+			})
+
+			containerHeader.addEventListener('mouseout', event => {
+				// console.debug('containerHeader mouseout')
+
+				// container.lastPosition = null
+				container.draggingEvent(event)
+
+				container.removeEventListener('mousemove', container.draggingEvent)
+			})
+
+			container.appendChild(containerHeader)
+
 			iframe.style = 'flex-grow: 1; border: none;'
 			// iframe.setAttribute('allow', '')
 			iframe.src = chrome.runtime.getURL('pages/popup.html')
-
-			// console.debug('window = ', window)
 
 			window.popupMessageListener = event => {
 				// console.debug('message event = ', event)
@@ -48,7 +93,8 @@ chrome.action.onClicked.addListener(async tab => {
 
 				if (event.origin != `chrome-extension://${chrome.runtime.id}`) return
 
-				container.style.setProperty('--border-color', event.data.colors.border)
+				container.style.borderColor = event.data.colors.border
+				containerHeader.style.background = container.style.borderColor
 			}
 			window.addEventListener('message', window.popupMessageListener)
 
