d0ae30e8a99981ec434893b56c790bd9f57a7f70
Alexander Popov <alex.wayfer@gmail.com>
2024-08-13 21:30:03 +0300

    Replace `rollup-plugin-multi-input` with `globSync.map` for `input` from docs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,17 +1,31 @@
-import multiInput from 'rollup-plugin-multi-input'
+import { globSync } from 'glob'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import notify from 'rollup-plugin-notify'
 
 export default {
-	input: ['scripts/src/**/*.js'],
+	//// https://rollupjs.org/configuration-options/#input
+	input: Object.fromEntries(
+		globSync('scripts/src/**/*.js').map(file => [
+			// This remove `src/` as well as the file extension from each
+			// file, so e.g. src/nested/foo.js becomes nested/foo
+			path.relative(
+				'scripts/src',
+				file.slice(0, file.length - path.extname(file).length)
+			),
+			// This expands the relative paths to absolute paths, so e.g.
+			// src/nested/foo becomes /project/src/nested/foo.js
+			fileURLToPath(new URL(file, import.meta.url))
+		])
+	),
 	output: {
 		format: 'es',
 		dir: 'scripts/compiled/'
 	},
 	plugins: [
-		// https://github.com/alfredosalzillo/rollup-plugin-multi-input/issues/61
-		multiInput({ relative: 'scripts/src/' }),
 		commonjs(),
 		resolve({
 			browser: true,
